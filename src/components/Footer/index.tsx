dd7468a7d83fd6c34b2afb21caace433c9fbc872
KantKontrol <nick.derissio.tech@gmail.com>
2023-04-07 16:41:13 -0400

    updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,41 +10,17 @@ import {
 
 const Footer = () => {
   return (
-    <MDBFooter bgColor="light" className="text-center text-lg-start text-muted">
-      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
-
-        <div>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
-      </section>
-
+    <MDBFooter
+      bgColor="light"
+      className="text-center text-lg-start text-muted fixed-bottom border-top"
+    >
       <section className="">
-        <MDBContainer className="text-center text-md-start mt-5">
+        <MDBContainer className="text-center text-md-start mt-4">
           <MDBRow className="mt-3">
-            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">
-                <MDBIcon icon="gem" className="me-3" />
-                Company name
+            <MDBCol md="4" lg="4" xl="3" className="mx-auto mb-3">
+              <h6 className="text-uppercase fw-bold mb-3">
+                <MDBIcon icon="code-branch" className="me-3" />
+                Nicholas DeRissio
               </h6>
               <p>
                 Here you can use rows and columns to organize your footer
@@ -53,50 +29,18 @@ const Footer = () => {
               </p>
             </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+            <MDBCol md="4" lg="3" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Social</h6>
               <p>
-                <a href="#!" className="text-reset">
-                  Angular
+                <a href="" className="me-2 text-reset">
+                  <MDBIcon fab icon="linkedin" className="me-2" />
+                  in/nicholas-derissio
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
-                  React
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Help
+                <a href="" className="me-2 text-reset">
+                  <MDBIcon fab icon="github" className="me-2" />
+                  KantKontrol
                 </a>
               </p>
             </MDBCol>
@@ -105,17 +49,11 @@ const Footer = () => {
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
+                North Fort Myers, FL 33903
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+                nick.derissio.tech@gmail.com
               </p>
             </MDBCol>
           </MDBRow>
@@ -126,10 +64,8 @@ const Footer = () => {
         className="text-center p-4"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
       >
-        © 2021 Copyright:
-        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
+        © 2023 Copyright:
+        <span className="text-reset fw-bold ms-1">nderissio.com</span>
       </div>
     </MDBFooter>
   );
